dee95ce4fbec3a0443744a37e25bfbe2a4c24464
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-09 00:52:36 +0100

    - Add Quote component - Test Quote component

---
+++ app/components/Quote/Quote.test.tsx
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Quote } from "./Quote";
+
+describe("Quote", () => {
+  it("renders the Quote", () => {
+    render(<Quote />);
+
+    const quote = screen.getByRole("article");
+
+    expect(quote).toBeInTheDocument();
+  });
+});
