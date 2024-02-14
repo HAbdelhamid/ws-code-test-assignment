e071f250f95ac78845f096f12ade6e1b554e4d8c
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-14 21:17:39 +0100

    Add Jest polyfills for Node.js globals

--- jest.config.ts
+++ jest.config.ts
@@ -142,7 +142,7 @@ const config: Config = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["./jest.polyfills.js"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
@@ -157,7 +157,9 @@ const config: Config = {
   testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
-  // testEnvironmentOptions: {},
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
 
   // Adds a location field to test results
   // testLocationInResults: false,
