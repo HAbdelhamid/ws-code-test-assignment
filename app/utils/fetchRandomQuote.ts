d4f95766e071e6bc92473c94b1f9ce2dafe0159f
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-14 21:46:45 +0100

    Update fetchRandomQuote to disable caching

--- app/utils/fetchRandomQuote.ts
+++ app/utils/fetchRandomQuote.ts
@@ -1,4 +1,4 @@
 export const fetchRandomQuote = () =>
-  fetch("https://api.chucknorris.io/jokes/random?category=dev")
-    .then((response) => response.json())
-    .then((data) => data.value);
+  fetch("https://api.chucknorris.io/jokes/random?category=dev", {
+    cache: "no-store",
+  }).then((response) => response.json());
