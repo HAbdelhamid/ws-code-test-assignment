e76aa9b38a15d1e4e663e4cd95e9fc6870d1a4b6
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-15 19:08:21 +0100

    Add SWRProvider to Footer component

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,15 +1,38 @@
+import { Suspense } from "react";
 import Counter from "../Counter/Counter";
+import { SWRProvider } from "../Providers/SWRProvider";
 
 /**
  * Footer component
  *
- * @todo add styles
  * @returns
  */
-const Footer = () => {
+const Footer = async () => {
+  let fallBackData = { count: 0 };
+  try {
+    const res = await fetch("http://localhost:3000/api/visitors", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    fallBackData = await res.json();
+  } catch (error) {
+    console.log("Error fetching data", error);
+  }
+
   return (
-    <footer>
-      <Counter />
+    <footer className="flex justify-center">
+      <SWRProvider
+        value={{
+          fallback: {
+            "/api/visitors": fallBackData,
+          },
+        }}
+      >
+        <Counter />
+      </SWRProvider>
     </footer>
   );
 };
