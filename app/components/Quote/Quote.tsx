61401f9ce6c22255441a1fc8f745f472d96e4e97
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-15 19:03:49 +0100

    Add image and fetch random quote button

--- app/components/Quote/Quote.tsx
+++ app/components/Quote/Quote.tsx
@@ -1,18 +1,52 @@
 import Image from "next/image";
+import { Icon } from "../Icon/Icon";
 
 /**
- * Quote component
+ * Quote dump component
  *
- * @todo call an API to get a random quote
- * @todo add a button to get a new quote
- * @todo add styles
- * @returns quote component
+ * @returns {JSX.Element} quote.
  */
-export const Quote = () => {
+export const Quote = ({
+  content,
+  className = "",
+  imageURL,
+  onFetchRandom,
+}: {
+  content: string;
+  className?: string;
+  imageURL?: string;
+  onFetchRandom?: () => void;
+}) => {
   return (
-    <article className="flex flex-col items-center">
-      <Image src="/images/quote.png" alt="Quote" width={100} height={100} />
-      <p className="text-2xl">Quote</p>
+    <article
+      className={`flex flex-col-reverse justify-between gap-10 items-center w-full xl2:flex-row ${className}`}
+    >
+      {imageURL && (
+        <Image
+          className="rounded-lg shadow-lg object-cover"
+          src={imageURL}
+          alt="Quote"
+          width={500}
+          height={500}
+        />
+      )}
+      <div className="flex flex-col gap-5 items-center  p-4 text-gray-800 bg-white rounded-lg">
+        <blockquote className="flex flex-row gap-5">
+          <Icon name="quote-up" />
+          <p className="text-1xl italic font-medium text-gray-600 text-justify">
+            {content}
+          </p>
+          <Icon name="quote-down" />
+        </blockquote>
+        {onFetchRandom && (
+          <button
+            className="bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded w-40"
+            onClick={() => onFetchRandom()}
+          >
+            Generate
+          </button>
+        )}
+      </div>
     </article>
   );
 };
