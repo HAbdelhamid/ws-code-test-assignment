a00550f68f05efe186a1806d5ee57e4fc2cc7448
abdelhamid hamza <abdelhamidhamzaa@gmail.com>
2024-02-15 19:20:25 +0100

    Add tests for Counter component and handle API request error

--- app/components/Counter/Counter.test.tsx
+++ app/components/Counter/Counter.test.tsx
@@ -1,15 +1,40 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import Counter from "./Counter";
+import { server } from "@/mock-server/server";
+import { HttpResponse, http } from "msw";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
 
 describe("Counter", () => {
-  it("renders the counter", () => {
+  it("renders the counter", async () => {
     render(<Counter />);
 
-    const counter = screen.getByText("Counter");
-
-    expect(counter).toBeInTheDocument();
+    const countElement = await screen.findByTestId("visitor-count");
+    expect(countElement).toBeInTheDocument();
   });
-  it.todo("calls bff to get the counter value");
-  it.todo("makes the number humanly readable");
+
+  it("renders error message when API request fails", async () => {
+    server.use(
+      http.get("/api/visitors", () => {
+        return HttpResponse.error();
+      })
+    );
+
+    render(<Counter />);
+
+    const error = await screen.getByText("Error");
+
+    expect(error).toBeInTheDocument();
+  });
 });
